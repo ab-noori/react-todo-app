06664cfdc5213d2e0a75fa5780b0d99acffcc51e
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-21 19:06:23 +0430

    Install react-router-dom and create routing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-// import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import './App.scss';
 
 function App() {
   return (
-    <h1>Todo App</h1>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/calculator" element={<h1>Calculator</h1>} />
+      <Route path="/quote" element={<h1>Quote</h1>} />
+    </Routes>
   );
 }
 
