8c8778fe940ece279a831a787e7fd6fb55207c93
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-02 23:09:35 +0330

    Add todo state to root component.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todos, setTodos] = useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  // we'll render our todos here ...
+  // return <div></div>
 }
 
 export default App;
